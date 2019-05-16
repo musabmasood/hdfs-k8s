d0c327a54589898da5dfbf9057481c768e87deb6
Austin Daelemans <austin.daelemans@b-yond.com>
2019-05-16 11:54:07 -0500

    BIP-891: Edits to ha compose for testing to reduce conflicts with other images

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ HADOOP_VERSION=$1
 fi
 
 
-docker build -t gcr.io/byond-infinity-platform/integrity-hdfs:${HADOOP_VERSION} ./base/ --build-arg HADOOP_VERSION=$HADOOP_VERSION
-docker build -t gcr.io/byond-infinity-platform/integrity-hdfs-namenode:${HADOOP_VERSION} ./namenode/ --build-arg HADOOP_VERSION=$HADOOP_VERSION
-docker build -t gcr.io/byond-infinity-platform/integrity-hdfs-datanode:${HADOOP_VERSION} ./datanode/ --build-arg HADOOP_VERSION=$HADOOP_VERSION
+docker build -t gcr.io/byond-infinity-platform/integrity-hdfs:${HADOOP_VERSION}-ha ./base/ --build-arg HADOOP_VERSION=$HADOOP_VERSION
+docker build -t gcr.io/byond-infinity-platform/integrity-hdfs-namenode:${HADOOP_VERSION}-ha ./namenode/ --build-arg HADOOP_VERSION=$HADOOP_VERSION
+docker build -t gcr.io/byond-infinity-platform/integrity-hdfs-datanode:${HADOOP_VERSION}-ha ./datanode/ --build-arg HADOOP_VERSION=$HADOOP_VERSION
