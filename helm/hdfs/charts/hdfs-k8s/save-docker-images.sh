32cff696cfe8481e2b81a804caa1f58833cb45e4
Alexis Ferrucci <alexis.ferrucci@b-yond.com>
2019-09-24 14:43:23 -0300

    BIP-2312 : Adding logic to support active node service.

--- helm/hdfs/charts/hdfs-k8s/save-docker-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e -x
-docker pull gcr.io/google_samples/k8szk:v3
-docker pull gcr.io/byond-infinity-platform/integrity-hdfs-namenode:2.9.2
-docker pull busybox:1.27.1
-docker pull gcr.io/byond-infinity-platform/integrity-hdfs-namenode:2.9.2
-docker pull gcr.io/byond-infinity-platform/integrity-hdfs:2.9.2
-docker pull gcr.io/byond-infinity-platform/integrity-hdfs-datanode:2.9.2
-docker save gcr.io/google_samples/k8szk:v3 gcr.io/byond-infinity-platform/integrity-hdfs-namenode:2.9.2 busybox:1.27.1 gcr.io/byond-infinity-platform/integrity-hdfs-namenode:2.9.2 gcr.io/byond-infinity-platform/integrity-hdfs:2.9.2 gcr.io/byond-infinity-platform/integrity-hdfs-datanode:2.9.2  | gzip -c > images.tar.gz
